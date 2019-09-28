c6c89d5a7280935285ffcb2b9431cf0673310e7f
Akhtar <reachtoakhtar@gmail.com>
2019-09-28 21:23:26 +0530

    Add docker configurations.

---
+++ api/ipl_stat/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IplStatConfig(AppConfig):
+    name = 'ipl_stat'
---
+++ api/ipl_stat/urls.py
@@ -0,0 +1,12 @@
+import logging
+from django.contrib import admin
+from django.urls import path, include
+
+__author__ = "akhtar"
+
+logger = logging.getLogger(__name__)
+
+
+urlpatterns = [
+    # path('ipl_stat/', include("ipl_stat.urls")),
+]

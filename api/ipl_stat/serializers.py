da658ddd383b83c3b1a334b649dfec1200a03f88
Akhtar <reachtoakhtar@gmail.com>
2019-09-29 11:06:20 +0530

    Complete all functionality.

--- api/ipl_stat/serializers.py
+++ api/ipl_stat/serializers.py
@@ -1,6 +1,10 @@
 import logging
+import math
 
+from django.db.models import Count, Q, Sum
 from rest_framework import serializers
+
+from ipl_stat.models import Delivery
 
 __author__ = "akhtar"
 
@@ -17,6 +21,28 @@ class TossWinSerializer(serializers.Serializer):
     no_of_wins = serializers.IntegerField()
 
 
+class TossWinDecisionSerializer(serializers.Serializer):
+    toss_winner = serializers.CharField(max_length=128)
+    no_of_wins = serializers.IntegerField()
+    field_times = serializers.IntegerField()
+    bat_times = serializers.IntegerField()
+    percentage_bat = serializers.SerializerMethodField()
+
+    def get_percentage_bat(self, value):
+        percentage = value["bat_times"] * 100 / value["no_of_wins"]
+        return math.ceil(percentage)
+
+
+class RunWinSerializer(serializers.Serializer):
+    winner = serializers.CharField(max_length=128)
+    max_win_margin = serializers.IntegerField()
+
+
+class WicketWinSerializer(serializers.Serializer):
+    winner = serializers.CharField(max_length=128)
+    max_win_wickets = serializers.IntegerField()
+
+
 class PlayerOfMatchSerializer(serializers.Serializer):
     player_of_match = serializers.CharField(max_length=128)
     no_of_times = serializers.IntegerField()
@@ -25,15 +51,60 @@ class PlayerOfMatchSerializer(serializers.Serializer):
 class WinnerLocationSerializer(serializers.Serializer):
     venue = serializers.CharField(max_length=128)
     winner = serializers.CharField(max_length=128)
+    city = serializers.CharField(max_length=128)
     no_of_times = serializers.IntegerField()
 
 
+class RunSerializer(serializers.Serializer):
+    match_id = serializers.IntegerField(source="id")
+    bowler_obj = serializers.SerializerMethodField()
+
+    def get_bowler_obj(self, value):
+        deliveries = Delivery.objects.filter(match_id=value["id"])
+        bowler_obj = deliveries.values("bowler").annotate(
+            runs_given=Sum("total_runs")).order_by("-runs_given").first()
+
+        return bowler_obj
+
+    def to_representation(self, instance):
+        value = super().to_representation(instance)
+        value["bowler"] = value["bowler_obj"]["bowler"]
+        value["runs_given"] = value["bowler_obj"]["runs_given"]
+        del value["bowler_obj"]
+        return value
+
+
+class CatchSerializer(serializers.Serializer):
+    match_id = serializers.IntegerField(source="id")
+    fielder_obj = serializers.SerializerMethodField()
+
+    def get_fielder_obj(self, value):
+        deliveries = Delivery.objects.filter(match_id=value["id"])
+        fielder_obj = deliveries.values("fielder").annotate(
+            catches_taken=Count("dismissal_kind", filter=Q(dismissal_kind="caught"))).order_by("-catches_taken").first()
+
+        return fielder_obj
+
+    def to_representation(self, instance):
+        value = super().to_representation(instance)
+        value["fielder"] = value["fielder_obj"]["fielder"]
+        value["catches_taken"] = value["fielder_obj"]["catches_taken"]
+        del value["fielder_obj"]
+        return value
+
+
 class StatSerializer(serializers.Serializer):
+    top_winner = WinSerializer()
     winners = serializers.SerializerMethodField()
     toss_winners = serializers.SerializerMethodField()
+    toss_and_match_winners = serializers.SerializerMethodField()
+    toss_winners_decision = serializers.SerializerMethodField()
+    run_winners = serializers.SerializerMethodField()
+    wicket_winners = serializers.SerializerMethodField()
     players_of_match = serializers.SerializerMethodField()
     most_win_location = serializers.SerializerMethodField()
-    top_winner = WinSerializer()
+    most_runs_given = serializers.SerializerMethodField()
+    most_catches_taken = serializers.SerializerMethodField()
 
     def get_winners(self, value):
         return WinSerializer(value["winners"], many=True).data
@@ -41,8 +112,26 @@ class StatSerializer(serializers.Serializer):
     def get_toss_winners(self, value):
         return TossWinSerializer(value["toss_winners"], many=True).data
 
+    def get_toss_and_match_winners(self, value):
+        return WinSerializer(value["toss_and_match_winners"], many=True).data
+
+    def get_toss_winners_decision(self, value):
+        return TossWinDecisionSerializer(value["toss_winners_decision"], many=True).data
+
+    def get_run_winners(self, value):
+        return RunWinSerializer(value["run_winners"], many=True).data
+
+    def get_wicket_winners(self, value):
+        return WicketWinSerializer(value["wicket_winners"], many=True).data
+
     def get_players_of_match(self, value):
         return PlayerOfMatchSerializer(value["players_of_match"], many=True).data
 
     def get_most_win_location(self, value):
         return WinnerLocationSerializer(value["most_win_location"], many=True).data
+
+    def get_most_runs_given(self, value):
+        return RunSerializer(value["most_runs_given"], many=True).data
+
+    def get_most_catches_taken(self, value):
+        return CatchSerializer(value["most_catches_taken"], many=True).data
--- api/ipl_stat/views.py
+++ api/ipl_stat/views.py
@@ -1,17 +1,14 @@
 import os
-from collections import OrderedDict
 
 import pandas as pd
-from django.db.models import Count
+from django.db.models import Count, Max, Q, F
 from rest_framework import status
 
-from rest_framework.generics import CreateAPIView, RetrieveAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.response import Response
 
 from ipl_stat.models import Delivery, Match
-from ipl_stat.serializers import (
-    StatSerializer, WinSerializer,
-    TossWinSerializer, PlayerOfMatchSerializer, WinnerLocationSerializer)
+from ipl_stat.serializers import StatSerializer
 
 
 class LookupCreateView(CreateAPIView):
@@ -50,17 +47,46 @@ class StatsView(ListAPIView):
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
 
-        winners = queryset.values("winner").annotate(no_of_wins=Count("winner")).order_by("-no_of_wins")[:4]
-        toss_winners = queryset.values("toss_winner").annotate(no_of_wins=Count("toss_winner")).order_by("-no_of_wins")[:4]
-        players_of_match = queryset.values("player_of_match").annotate(no_of_times=Count("player_of_match")).order_by("-no_of_times")[:4]
-        most_win_location = queryset.values("venue", "winner").filter(winner=winners[0]["winner"]).annotate(no_of_times=Count("venue")).order_by("-no_of_times")[:4]
+        winners = queryset.values("winner").annotate(
+            no_of_wins=Count("winner")).order_by("-no_of_wins")
+
+        toss_winners = queryset.values("toss_winner").annotate(
+            no_of_wins=Count("toss_winner")).order_by("-no_of_wins")
+
+        toss_winners_decision = queryset.values("toss_winner").annotate(
+            no_of_wins=Count("toss_winner"),
+            field_times=Count("toss_decision", filter=Q(toss_decision="field")),
+            bat_times=Count("toss_decision", filter=Q(toss_decision="bat"))
+        )
+
+        run_winners = queryset.values("winner").annotate(
+            max_win_margin=Max("win_by_runs")).order_by("-max_win_margin")
+
+        wicket_winners = queryset.values("winner").annotate(
+            max_win_wickets=Max("win_by_wickets")).order_by("-max_win_wickets")
+
+        toss_and_match_winners = queryset.values("winner").annotate(
+            no_of_wins=Count("winner", filter=Q(toss_winner=F("winner")))).order_by("-no_of_wins")
+
+        players_of_match = queryset.values("player_of_match").annotate(
+            no_of_times=Count("player_of_match")).order_by("-no_of_times")
+
+        most_win_location = queryset.values("venue", "winner", "city").filter(
+            winner=winners[0]["winner"]).annotate(
+            no_of_times=Count("venue")).order_by("-no_of_times")
 
         serializer_data = {
             "winners": winners,
             "top_winner": winners[0],
             "toss_winners": toss_winners,
+            "toss_and_match_winners": toss_and_match_winners,
+            "toss_winners_decision": toss_winners_decision,
+            "run_winners": run_winners,
+            "wicket_winners": wicket_winners,
             "players_of_match": players_of_match,
-            "most_win_location": most_win_location
+            "most_win_location": most_win_location,
+            "most_runs_given": queryset.values("id"),
+            "most_catches_taken": queryset.values("id")
         }
         data = self.serializer_class(serializer_data).data
         return Response(data=data, status=status.HTTP_200_OK)
